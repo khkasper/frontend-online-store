735803522e94ff265773c5d1039b78b49a01dfee
Gabriella Duarte <78912656+gsdu-arte@users.noreply.github.com>
2021-09-18 16:54:20 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import ProductList from './pages/ProductList';
-import ShoppingCart from './pages/ShoppingCart'
+import ShoppingCart from './pages/ShoppingCart';
 
 function App() {
   return (
